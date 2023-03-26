1d1f402faf58b16f53a8a5ff1c0499104922a7e8
Erik Roth <erikroth@rltechs.com>
2023-03-26 16:04:52 -0600

    add xbox controller assignned values

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -15,3 +15,36 @@
 // Include boolean capabilities
 #include <stdbool.h>
 
+
+
+// System XBOX 360/One Controller Values
+namespace ConstXbc {
+
+    // Axis Controlled Inputs
+    constexpr int LEFT_JOYSTICK_X = 0;    
+    constexpr int LEFT_JOYSTICK_Y = 1;
+    constexpr int LEFT_TRIGGER_ID = 2;
+    constexpr int RIGHT_TRIGGER_ID = 3;
+    constexpr int RIGHT_JOYSTICK_X = 4;    
+    constexpr int RIGHT_JOYSTICK_Y = 5;
+
+    // Button Controlled Inputs
+    constexpr int A = 1;
+    constexpr int B = 2;
+    constexpr int X = 3;
+    constexpr int Y = 4;
+    constexpr int LEFT_BUMPER = 5;
+    constexpr int RIGHT_BUMPER = 6;
+    constexpr int SELECT = 7;
+    constexpr int START = 8;
+    constexpr int LEFT_JOYSTICK = 9;
+    constexpr int RIGHT_JOYSTICK = 10;
+
+    // CREATE A WIDGEt FOR THESE TO CHANGE
+
+    //Dead zone
+    constexpr double DEAD_ZONE = 0.5; //approximate value
+
+    //Driver controller Port
+    constexpr int DRIVER_CONTROLLER_PORT = 0;
+}
